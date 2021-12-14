d031c7e43c583c69e2402746e34cea0c93fe6f82
Dane Hixson <dhixson@cove-productions.com>
2021-12-14 10:41:02 -0700

    initial app commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+// fastify server with ping enpoint
+import fastify from 'fastify';
+
+const port = Number(process.env.PORT) || 8080
+
+const server = fastify({
+    logger: true,
+});
+
+server.get('/', (request, reply) => {
+    reply.send({ hello: 'world2' });
+});
+
+server.listen(port, function (err, address) {
+    if (err) {
+        console.error(err)
+        process.exit(1)
+    }
+    console.log(`Server listening on ${address}`)
+})interface
